f24eabcb026b0bc75dfe48bab34248f75c43d735
Nate Hill <natanio@gmail.com>
2014-10-08 09:38:30 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mrenglish::Application.config.secret_key_base = 'ac35d57b2bf6d65f8740d4ccc9d42e6e0baa7029fccf4f1c3cb4ef02aa4b0a6d270614369bdde364b81b2bf98a7588bace98dea3972716c792e9c59171e604ca'
